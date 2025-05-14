01e0d26b8f564fcf9e2d60a1928e4355b4f8c2fa
Santosh-Nagh <santoshnagh1@gmail.com>
2025-05-14 06:06:32 -0500

    Fix: Tailwind config + app entrypoint update for styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css';
+// src/pages/_app.tsx
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { OrderProvider } from '@/context/OrderContext';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <OrderProvider>
+      <Component {...pageProps} />
+    </OrderProvider>
+  );
 }
